6796a19bc273e0381a948c60e7a000c97ddbdfc1
mwitt20 <mwitt20@andover.edu>
2019-12-13 13:33:55 -0500

    add Project Euler prime problem

---
+++ ProjectEulerPrime.playground/Contents.swift
@@ -0,0 +1,24 @@
+//:What is the 10,001st prime number?
+//:Ryan talked me through this problem and helped me understand it
+var primeNums: [Int] = [Int]()
+var testingNumber = 2
+
+func isPrime(num: Int, primeNums: [Int]) -> Bool {
+    for i in 0..<primeNums.count {
+        if num % primeNums[i] == 0 {
+            return false
+        }
+    }
+    return true
+}
+
+while primeNums.count < 10_001 {
+    if isPrime(num: testingNumber, primeNums: primeNums) {
+        primeNums.append(testingNumber)
+        testingNumber += 1
+    }
+}
+
+print(primeNums.last!)
+
+
