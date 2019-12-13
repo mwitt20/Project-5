e3aa185251c1019724044e57e9a6d220a71aee5a
mwitt20 <mwitt20@andover.edu>
2019-12-13 13:13:28 -0500

    Coding Bat Array-1 and Array-2 problems

---
+++ CodingBatArray-1.playground/Contents.swift
@@ -0,0 +1,34 @@
+//:Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+func firstOrLast6(array: [Int]) -> Bool {
+    if array[0] == 6 || array[array.count - 1] == 6 {
+        return true
+    }
+    return false
+}
+
+let test1 = firstOrLast6(array: [1,2,3])
+print(test1)
+
+let test2 = firstOrLast6(array: [6,2])
+print(test2)
+
+let test3 = firstOrLast6(array: [1,2,3,5,7,8,6])
+print(test3)
+
+let test4 = firstOrLast6(array: [1,6,3])
+print(test4)
+
+
+//:Given an array of ints, swap the first and last elements in the array. Return the modified array. The array length will be at least 1.
+var array = [1,2,3,4,5]
+
+func swapEnds(for: [Int]) {
+    array.append(array.first!)
+    array.insert(array[array.count - 2], at: 0)
+    array.remove(at: 1)
+    array.remove(at: array.count - 2)
+}
+
+swapEnds(for: array)
+print(array)
+
---
+++ CodingBatArray-2.playground/Contents.swift
@@ -0,0 +1,30 @@
+//:Return the number of even ints in the given array.
+var array = [1,2,3,4,5,6,7,8]
+var count = 0
+
+func numberOfEvens(for: [Int]) -> Int {
+    for i in array {
+        if i % 2 == 0 {
+            count += 1
+        }
+    }
+    return count
+}
+print("\(numberOfEvens(for: array)) even ints")
+
+
+//:Given an array of ints, return true if every 2 that appears in the array is next to another 2.
+
+var nums = [3,4,4,2,2,2,5]
+
+func areTheTwosPaired(for: [Int]) -> Bool{
+    for num in nums {
+        if nums[num] == 2 {
+            if nums[num-1] != 2 && nums[num+1] != 2{
+                return false
+            }
+        }
+    }
+    return true
+}
+print(areTheTwosPaired(for: nums))
