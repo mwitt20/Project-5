386ee29c4f1a3b2c9d33e92203c40a589b9ea4fa
mwitt20 <mwitt20@andover.edu>
2019-12-15 23:00:29 -0500

    edit CodingBatArray-3 and ProjecEulerPrime

--- CodingBatArray-3.playground/Contents.swift
+++ CodingBatArray-3.playground/Contents.swift
@@ -12,10 +12,10 @@ func doesTheArrayHaveEqualSides(array: [Int]) -> Bool {
         }
     }else{
         for i in array[0...(array.count / 2)] {
-            firstHalfSum += 1
+            firstHalfSum += i
         }
         for i in array[(array.count / 2)...array.count - 1] {
-            secondHalfSum += 1
+            secondHalfSum += i
         }
     }
     if firstHalfSum == secondHalfSum {
@@ -26,17 +26,27 @@ func doesTheArrayHaveEqualSides(array: [Int]) -> Bool {
 print(doesTheArrayHaveEqualSides(array: [1,2,2,1,3]))
 
 //:Return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is immediately followed by a 4. Do not move the 3's, but every other number may move. The array contains the same number of 3's and 4's, every 3 has a number after it that is not a 3, and a 3 appears in the array before any 4.
-var array = [1,2,3,5,4,4,3,2,3,2,4]
-var newArray = array.filter { $0 != 4} //https://iswift.org/cookbook/remove-item-from-array-by-value -remove all of the '4's
+var array = [1,2,3,5,4,4,3]
 
-func fix34(for: [Int]) {
-    for i in newArray {
-        if newArray[i] == 3 {
-            newArray.insert(4, at: i + 1)
+func remove4(for: [Int]) -> [Int]{
+    for i in array {
+        if array[i] == 4 {
+            array.remove(at: i)
         }
     }
+    return array
 }
-fix34(for: newArray)
-print(newArray)
+
+func add4(for: [Int]) -> [Int] {
+    for i in array {
+        if array[i] == 3 {
+            array.insert(4, at: i + 1)
+        }
+    }
+    return array
+}
+remove4(for: array)
+add4(for: array)
+print(array)
 
 
