ef3cae1890c20e9f39cca8060854042ac9412f73
mwitt20 <mwitt20@andover.edu>
2019-12-16 01:18:25 -0500

    add links to array-3

--- CodingBatArray-3.playground/Contents.swift
+++ CodingBatArray-3.playground/Contents.swift
@@ -1,3 +1,4 @@
+//https://codingbat.com/prob/p158767
 //:Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers on one side is equal to the sum of the numbers on the other side.
 var firstHalfSum = 0
 var secondHalfSum = 0
@@ -25,6 +26,7 @@ func doesTheArrayHaveEqualSides(array: [Int]) -> Bool {
 }
 print(doesTheArrayHaveEqualSides(array: [1,2,2,1,3]))
 
+//https://codingbat.com/prob/p159339
 //:Return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is immediately followed by a 4. Do not move the 3's, but every other number may move. The array contains the same number of 3's and 4's, every 3 has a number after it that is not a 3, and a 3 appears in the array before any 4.
 var array = [1,2,3,5,4,4,3]
 
@@ -48,5 +50,6 @@ func add4(for: [Int]) -> [Int] {
 remove4(for: array)
 add4(for: array)
 print(array)
+
 
 
